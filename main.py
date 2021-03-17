2df4edaa3eb77abf39edc85ccf1813e5656a14bd
badr385 <59146833+badr385@users.noreply.github.com>
2021-03-17 12:03:57 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import os
+import random
+import time
+
+client = discord.Client()
+list =['Echo', 'Jwick', 'Mert', 'Magni', 'yagiz', 'Zeus', 'zico', 'Reis', 'Cihan']
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+@client.event
+async def on_message(message):
+    if message.content.startswith('!draw'):
+      await message.channel.send("List of people on draw are : \n" )
+      await message.channel.send('\n'.join(map(str, list)))
+      await message.channel.send("Please wait draw is in process")
+      time.sleep(10)
+      await message.channel.send("######################Draw is over winner appear######################")
+      Winner = random.choice(list)
+      await message.channel.send("Congratulation you are the winner of the giveaway this month march <3, is Mr " + Winner)
+      await message.channel.send("Can you provide us please your plateforme / Ios or Google? please " + Winner)
+
+client.run(os.getenv('TOKEN'))
